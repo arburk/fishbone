039cb1177308b6a2f770c4e28171bedc3ef2c407
Arno Burkhart <arno.burkhart@gmail.com>
2020-01-16 21:10:14 +0100

    created basic structure via start.spring.io

---
+++ src/test/java/org/arburk/fishbone/FishboneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arburk.fishbone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishboneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
