80ab6d8f610919fbc9ab27b67627d27ea9a5ea31
Arno Burkhart <arno.burkhart@gmail.com>
2020-02-13 17:42:21 +0100

    added basic unit test

---
+++ src/test/java/org/arburk/fishbone/controller/FishControllerTest.java
@@ -0,0 +1,35 @@
+package org.arburk.fishbone.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FishControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void rootNotFound() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(404));
+    }
+
+    @Test
+    void getFishHappyCase() throws Exception {
+        String testResult = "[{\"id\":0,\"names\":null,\"description\":null}]";
+        mvc.perform(MockMvcRequestBuilders.get("/fish").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(testResult)));
+    }
+}
